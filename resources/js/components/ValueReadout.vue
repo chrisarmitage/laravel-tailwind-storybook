c4c99212ae0eb23201c5d0b012d8d39eff40c18a
Chris Armitage <dev@chrisarmitage.com>
2019-09-04 09:49:26 +0100

    Add AppButton, accepts a 'value' prop and displays it

---
+++ resources/js/components/ValueReadout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Value: <span>{{ value }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ValueReadout",
+    props: {
+      value: {
+        type: Number,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
