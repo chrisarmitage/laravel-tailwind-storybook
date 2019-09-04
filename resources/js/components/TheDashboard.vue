418cb0a3eded47fdc7dd0202e08aa8810dd7ba5a
Chris Armitage <dev@chrisarmitage.com>
2019-09-04 09:50:52 +0100

    Add TheDashboard (single-instance component), holds the data for 'value' and methods to increment and decrement

---
+++ resources/js/components/TheDashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <app-button @click="increment()">+1</app-button>
+    <app-button @click="decrement()">-1</app-button>
+    <value-readout :value="value"></value-readout>
+  </div>
+</template>
+
+<script>
+  import AppButton from "./AppButton";
+  import ValueReadout from "./ValueReadout";
+  export default {
+    name: "TheDashboard",
+    components: {ValueReadout, AppButton},
+    data() {
+      return {
+        value: 10,
+      }
+    },
+    methods: {
+      increment() {
+        this.value++;
+      },
+
+      decrement() {
+        this.value--;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
